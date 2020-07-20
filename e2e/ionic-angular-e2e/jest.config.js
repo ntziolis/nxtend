f1bba3b5a1a64b656492d5b004f6e674f6038730
Devin Shoemaker <devinshoe@gmail.com>
2020-07-19 19:04:18 -0500

    feat(ionic-angular): generate initial ionic-angular plugin

---
+++ e2e/ionic-angular-e2e/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'ionic-angular-e2e',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/e2e/ionic-angular-e2e',
+};
---
+++ packages/ionic-angular/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'ionic-angular',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/packages/ionic-angular',
+};
